fb04de7a40bef19f76aead60c48268c8ad6ad813
Brandon May <battleb2014@gmail.com>
2019-11-30 16:15:19 -0500

    more  changes

--- social_cards/src/App.js
+++ social_cards/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return(
     <div className="container">
-                <img id="avatar" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_OYLYfaa1NsfEolg1ry9traAmC_5zUxip1Sz_a1mxmItEY5JbYQ&s'></img>
+                <img id="avatar" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_OYLYfaa1NsfEolg1ry9traAmC_5zUxip1Sz_a1mxmItEY5JbYQ&s' alt=''></img>
                 <div className="info">
                     <h2>Name: Emily Rose</h2>
                     <h2>Location: Georgia</h2>
