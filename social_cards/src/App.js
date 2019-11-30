55fe819f1a97d1512ed7a7d6ad1a9537758ada02
Brandon May <battleb2014@gmail.com>
2019-11-30 14:57:34 -0500

    changes

--- social_cards/src/App.js
+++ social_cards/src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className='container'>
-      <img id='avatar' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_OYLYfaa1NsfEolg1ry9traAmC_5zUxip1Sz_a1mxmItEY5JbYQ&s'></img>
-      <div className='info'>
-        <h2>User: Emily Rose</h2>
-        <h2>Location: Georgia</h2>
-        <h2>Age: 23</h2>
-      </div>
-    </div>
-  );
+  return(
+    <div className="container">
+                <img id="avatar" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_OYLYfaa1NsfEolg1ry9traAmC_5zUxip1Sz_a1mxmItEY5JbYQ&s'></img>
+                <div className="info">
+                    <h2>Name: Emily Rose</h2>
+                    <h2>Location: Georgia</h2>
+                    <h2>Age: 23</h2>
+                </div>
+            </div>  
+  )
 }
 
 export default App;
