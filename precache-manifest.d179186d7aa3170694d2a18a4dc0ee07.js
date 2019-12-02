f375a24b012e08703737c215ed48cbadd3c09628
Brandon May <battleb2014@gmail.com>
2019-12-01 22:02:10 -0500

    Updates

---
+++ precache-manifest.d179186d7aa3170694d2a18a4dc0ee07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04949782f61b9bb6d84ba9d4636e022e",
+    "url": "/reactSocialCard/index.html"
+  },
+  {
+    "revision": "934c1985f8c44e5a848e",
+    "url": "/reactSocialCard/static/css/main.d6140f67.chunk.css"
+  },
+  {
+    "revision": "ad107f472f6133583d70",
+    "url": "/reactSocialCard/static/js/2.cc67de9a.chunk.js"
+  },
+  {
+    "revision": "934c1985f8c44e5a848e",
+    "url": "/reactSocialCard/static/js/main.806a823f.chunk.js"
+  },
+  {
+    "revision": "55ebe7dbbfedba454476",
+    "url": "/reactSocialCard/static/js/runtime-main.77ba4080.js"
+  }
+]);
